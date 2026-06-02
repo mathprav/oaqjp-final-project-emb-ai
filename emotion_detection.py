f6908f5bfdaf454dd13f209517524ad7f00abd07
mathprav <mathprav@gmail.com>
2026-06-02 05:52:49 -0400

    3b partial

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,4 +5,20 @@ def emotion_detector(text_to_analyze):
     header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj= { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']["anger"]
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']["disgust"]
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']["fear"]
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']["joy"]
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']["sadness"]
+    
+    dominant_emotion = max(formatted_response['emotionPredictions'][0]['emotion'],key=formatted_response['emotionPredictions'][0]['emotion'].get)
+    
+    return {'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': dominant_emotion
+    }
+    #return response.text
