704cd324a397b638059666aa012cfb0edac5fdf1
mathprav <mathprav@gmail.com>
2026-06-02 21:39:11 -0400

    3b_formatted_text

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,12 +6,12 @@ def emotion_detector(text_to_analyze):
     myobj= { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']["anger"]
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']["disgust"]
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']["fear"]
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']["joy"]
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']["sadness"]
-    
+    anger_score     = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score   = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score      = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score       = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score   = formatted_response['emotionPredictions'][0]["emotion"]["sadness"]
+
     dominant_emotion = max(formatted_response['emotionPredictions'][0]['emotion'],key=formatted_response['emotionPredictions'][0]['emotion'].get)
     
     return {'anger': anger_score,
